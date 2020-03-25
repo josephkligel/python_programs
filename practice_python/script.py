b6f3c7bf08337e6835fa4ef22277248e2bc2763d
Joseph Kligel <jkligel@gmail.com>
2020-03-24 21:02:53 -0500

    Using bash script to push all updates

--- practice_python/script.py
+++ practice_python/script.py
@@ -1,14 +1,18 @@
+class TeaException(Exception):
+    def __init__(self, arg):
+        self.msg = arg
+
 class Tea:
     def __init__(self, temperature):
         self.__temperature = temperature
 
     def drink_tea(self):
         if self.__temperature > 85:
-            raise ValueError("Tea is too hot.")
+            raise TeaException("Tea is too hot.")
         elif self.__temperature < 65:
-            print("Too cold to drink.")
+            raise TeaException("Too cold to drink.")
         else:
             print("The temperature is just right. You can drink the tea!")
 
-cup = Tea(89)
+cup = Tea(50)
 cup.drink_tea()
