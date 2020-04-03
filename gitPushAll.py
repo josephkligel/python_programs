5c615dcb121fac7d49a683f61a65e28eff4c1c26
Joseph Kligel <jkligel@gmail.com>
2020-04-03 14:29:39 -0500

    generic message generated from gitPushAll python program

--- gitPushAll.py
+++ gitPushAll.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 from os import *
-import time
-
-add = "git add ."
-commit = "git commit -m 'generic message'"
-push = "git push -u origin master"
+from io import StringIO as sio
+from sys import stdin
 
 system("%s" % ("git add ."))
 system("%s" % ("git commit -m 'generic message generated from gitPushAll python program'"))
+
+stdin = sio("zigjag")
 system("%s" % ("git push -u origin master"))
