3346d70b9b8b25ec0af4539fed86304c707a75b0
Joseph Kligel <jkligel@gmail.com>
2020-04-08 23:20:52 -0500

    generic message generated from gitPushAll python program

--- sql/main.py
+++ sql/main.py
@@ -3,7 +3,7 @@ import sqlite3
 con = sqlite3.connect('sample.db')
 cObj = con.cursor()
 
-cObj.execute("""CREATE TABLE employees(
+cObj.execute("""CREATE TABLE IF NOT EXISTS employees(
     id INTEGER PRIMARY KEY,
     name TEXT,
     salary REAL,
@@ -11,6 +11,30 @@ cObj.execute("""CREATE TABLE employees(
     position TEXT
     )""")
 con.commit()
+
+def insert_value(id, name, salary, department, position):
+    cObj.execute("""
+    INSERT INTO employees
+    VALUES(?, ?, ?, ?, ?)
+    """, (id, name, salary, department, position))
+    con.commit()
+
+def update_department(dep, id):
+    cObj.execute("UPDATE employees SET department=? WHERE id=?", (dep, id))
+    con.commit()
+
+def sql_fetch():
+    cObj.execute("SELECT * FROM employees")
+    result = cObj.fetchall()
+    for item in result:
+        print(item)
+
+def delete_all():
+    cObj.execute("DELETE FROM employees")
+    con.commit()
+
+# delete_all()
+insert_value(1, 'Joseph', 75000, 'Python', 'Developer')
 
 cObj.close()
 con.close()
