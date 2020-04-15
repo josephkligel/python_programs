c1b61aed249e188ff07f332bfa69d044fafc945b
Joseph Kligel <jkligel@gmail.com>
2020-04-14 20:17:49 -0500

    generic message generated from gitPushAll python program

--- practice_python/pushwaiting.py
+++ practice_python/pushwaiting.py
@@ -8,5 +8,5 @@ def pushing():
 
 thread = threading.Thread(target=pushing)
 x = os.system('gitPushAll.py')
-if (x == "Username for 'https://github.com'"):
+if (x == "Username for 'https://github.com:'"):
     event.set()
