bf27f28ad524a862e131d0adc86804c29a5de330
Joseph Kligel <jkligel@gmail.com>
2020-04-15 02:06:42 -0500

    generic message generated from gitPushAll python program

--- practice_python/child.py
+++ practice_python/child.py
@@ -2,14 +2,13 @@ import sys, subprocess
 from pexpect import popen_spawn, spawn
 import pexpect
 
-# cmd = "echo 'Type yes or no: '"
-# child = spawn('ls -l')
-# child.expect('Type yes or no: ')
-# print(child.)
-# child.sendline('yes')
 
-cmd = 'echo zigjag | gitPushAll.py'.split()
-print(cmd)
-p = subprocess.Popen(cmd, shell=True)
+child = pexpect.spawn('gitPushAll.py')
+child.expect("Username for 'https://github.com':")
+child.sendline('zigjag')
+
+# cmd = 'echo zigjag | gitPushAll.py'.split()
+# print(cmd)
+# p = subprocess.Popen(cmd, shell=True)
 # p.communicate(input='\nzigjag', timeout=10)
 # p = subprocess.check_output('gitPushAll.py', input=b'zigjag')
