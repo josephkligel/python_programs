07c3605b2552b0638eeabb7ed13619443051b58a
Joseph Kligel <jkligel@gmail.com>
2020-04-15 02:36:54 -0500

    generic message generated from gitPushAll python program

--- practice_python/child.py
+++ practice_python/child.py
@@ -6,7 +6,7 @@ try:
     child = pexpect.spawn("gitPushAll.py")
     child.expect("Username.+: ")
     child.sendline('zigjag')
-    print(child.after)
+    # print(child.after)
     child.expect('Password.+: ')
     child.sendline('wrong')
     child.logfile = open("./mylog", 'w')
