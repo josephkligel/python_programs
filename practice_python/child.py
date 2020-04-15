12453a6be4103f5bf3f5111729619322a8c55c63
Joseph Kligel <jkligel@gmail.com>
2020-04-15 02:05:46 -0500

    generic message generated from gitPushAll python program

--- practice_python/child.py
+++ practice_python/child.py
@@ -8,7 +8,8 @@ import pexpect
 # print(child.)
 # child.sendline('yes')
 
-cmd = 'gitPushAll.py | zigjag'.split()
-p = subprocess.Popen(cmd)
+cmd = 'echo zigjag | gitPushAll.py'.split()
+print(cmd)
+p = subprocess.Popen(cmd, shell=True)
 # p.communicate(input='\nzigjag', timeout=10)
 # p = subprocess.check_output('gitPushAll.py', input=b'zigjag')
