c47311542f134c2b2ccb0d1fd8b1519d33137fe9
Joseph Kligel <jkligel@gmail.com>
2020-05-21 01:14:20 -0500

    generic message generated from gitPushAll python program

---
+++ software-finished_products/templates.py
@@ -0,0 +1,29 @@
+import sys, argparse, re
+
+parser = argparse.ArgumentParser(description='Generate templates for html, python, bash, or C. Example: "python templates.py --html template.html"')
+parser.add_argument('--html', help='Create an html file template')
+parser.add_argument('-p', '--python', help='Create a python file template')
+parser.add_argument('-c', '--cpro', help='Create a C programming template')
+parser.add_argument('-b', '--bash', help='Create a bash template')
+args = parser.parse_args()
+
+def create_template():
+    if sys.argv[2] == '':
+        sys.argv[2] == 'generated_template'
+    if args.html:
+        with open(sys.argv[2] + '.html', 'w') as fh:
+            fh.write('<html>\n</html>')
+    elif args.python:
+        with open(sys.argv[2] + '.py', 'w') as fh:
+            fh.write('#!/usr/bin/env python')
+    elif args.cpro:
+        with open(sys.argv[2] + '.c', 'w') as fh:
+            fh.write("""#include <stdio.h>\n
+int main(int argc, int *argv[]){
+\n\treturn(0)
+}""")
+    elif args.bash:
+        with open(sys.argv[2] + '.sh', 'w') as fh:
+            fh.write("""#!/usr/bin/env bash""")
+
+create_template()
