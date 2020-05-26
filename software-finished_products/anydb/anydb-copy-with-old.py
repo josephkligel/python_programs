1065253a62f3342d05459efed83e33e0ec51a566
Joseph Kligel <jkligel@gmail.com>
2020-05-26 16:41:50 -0500

    Using bash script to push all updates

--- software-finished_products/anydb/anydb-copy-with-old.py
+++ software-finished_products/anydb/anydb-copy-with-old.py
@@ -24,11 +24,8 @@ class App(Tk):
         if os.path.exists('./last_database.log'):
             with open('./last_database.log', 'r') as fr:
                 last_database = fr.readline().strip()
-            self.database_box.insert(1.0, last_database)
+            self.database.config(text=last_database)
             self.get_connection(last_database)
-
-        #Last thing to do when initializing
-        self.create_style()
     
     def create_menu(self):
         menubar = Menu(self)
@@ -36,24 +33,37 @@ class App(Tk):
         self.config(menu=menubar)
 
     def create_buttons(self, parent):
-        self.database_box = Text(parent, height=1)
-        self.database_box.grid(row=0, column=0, padx=10, pady=10)
+        self.database = Label(parent)
+        self.database.grid(row=0, column=0)
         openBtn = ttk.Button(parent, text='Choose Db')
-        openBtn.grid(row=0, column=1, padx=10, pady=10, sticky='e')
+        openBtn.grid(row=0, column=1)
         openBtn.config(command=self.choose_file)
 
         self.table_var = StringVar()
         self.combobox = ttk.Combobox(parent, textvariable=self.table_var)
-        self.combobox.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky='we')
+        self.combobox.grid(row=1, column=0, columnspan=2)
         self.populate_button = ttk.Button(parent, text='Populate Table',  command=lambda: self.populate_table(self.tab2, self.table_var.get()))
-        self.populate_button.grid(row=2, column=1, padx=10, pady=10, sticky='e')
+        self.populate_button.grid(row=2, column=1, sticky='e')
 
     def choose_file(self):
         database_dialog = filedialog.askopenfile(initialdir='./')
-        self.database_box.insert(1.0, database_dialog.name)
+        self.database.config(text=database_dialog.name)
         with open('last_database.log', 'w') as fh:
             fh.write(f'{database_dialog.name}\n')
         self.get_connection(database_dialog.name)
+
+    def old_get_connection(self, parent, database):#TODO: delete later
+        self.conn = sqlite3.Connection(database)
+        self.cursor = self.conn.cursor()
+        get_tables_statement = self.cursor.execute('SELECT name FROM sqlite_master WHERE type="table";')
+        tables = []
+        for name in get_tables_statement:
+            tables.append(name[0])
+        self.combobox.config(values=tables)
+        #try :
+           # self.combobox.bind('<<ComboboxSelected>>', lambda e: self.populate_table(self.tab2, self.table_name.get()))
+        #except sqlite3.OperationalError as oe:
+         #   print('Could not retrieve table name')
 
     def get_connection(self, database):
         self.db = Database(database)
@@ -65,7 +75,7 @@ class App(Tk):
         #Create Header
         row, column = 0, 0
         for name in self.column_names:
-            Label(parent, text=name, bg='#142E54', fg='white', relief='groove',
+            Label(parent, bg='#142E54', text=name, fg='white', relief='groove',
                 font='lato 10 bold', borderwidth=1).grid(row=row, column=column, sticky='nsew')
             column += 1
 
@@ -73,7 +83,7 @@ class App(Tk):
         row, column = 1, 0
         for data_row in self.all:
             for data_cell in data_row:
-                cell = Entry(parent, borderwidth=2, relief='groove', font='lato 10')
+                cell = Entry(parent, bg="White", fg="Black", borderwidth=2, relief='groove', font='lato 10')
                 cell.grid(row=row, column=column, sticky='nsew')
                 cell.insert(0, data_cell)
                 cell.configure(state='readonly')
@@ -81,35 +91,6 @@ class App(Tk):
             row += 1
             column = 0
 
-        #Add text entries and update, delete, and insert buttons
-        crud_vars = []
-        for i in range(len(self.column_names)):
-            crud_var = StringVar()
-            crud_vars.append(crud_var)
-            Entry(parent, textvariable=crud_var).grid(row=row, column=i)
-        
-        def call_crud_vars(func):
-            an_array = [var.get() for var in crud_vars]
-            func(self.table_var.get(), an_array)
-            self.refresh(parent)
-            
-        deleteBtn = ttk.Button(parent, text='Delete', command=lambda: call_crud_vars(self.db.delete_data))
-        deleteBtn.grid(row=row+1, column=i-2, sticky='e')
-        updateBtn = ttk.Button(parent, text='Update', command=lambda: call_crud_vars(self.db.update_data))
-        updateBtn.grid(row=row+1, column=i-1, stick='e')
-        insertBtn = ttk.Button(parent, text='Insert', command=lambda: call_crud_vars(self.db.insert_data))
-        insertBtn.grid(row=row+1, column=i, sticky='e')
-
-        self.notebook.select(1)
-
-    def create_style(self):
-        style = ttk.Style()
-        style.configure('TButton', foreground='white', background='#142E54')
-
-    def refresh(self, parent):
-        for frame in parent.winfo_children():
-            frame.destroy()
-        self.populate_table(parent, self.table_var.get())
 
 if __name__ == '__main__':
     app = App()
