56039f9f940a68348f7636bd2e676a49531f4453
Joseph Kligel <jkligel@gmail.com>
2020-06-21 05:47:37 -0500

    Using bash script to push all updates

--- compileC++.py
+++ compileC++.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
-import os, sys
+import os
+import sys
+import argparse
 
 print(f'Compiling...\n')
 os.system(f'g++ -Wall {sys.argv[1]} -o {sys.argv[2]}')
 
-print('Running...\n\n')
-os.system(f'./{sys.argv[2]}')
+for arg in sys.argv:
+    if arg == '--run':
+        print('Running...\n')
+        os.system(f'./{sys.argv[2]}')
