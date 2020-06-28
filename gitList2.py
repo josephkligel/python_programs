a928d6f692104c879980af4fed687657a4899911
Joseph Kligel <jkligel@gmail.com>
2020-06-28 07:54:10 -0500

    Using bash script to push all updates

--- gitList2.py
+++ gitList2.py
@@ -1,9 +1,9 @@
 import os, glob
 
 def find_git(search_dir='/home/jkligel/Github'):
-    script_file = open('pushAll.sh', 'w')
+    script_file = open('/home/jkligel/bin/pushAll.sh', 'w')
     
-    script_file.writelines(['#!/usr/bin/env python\n', 'git config --global credentials.helper "cache --timeout 7200"\n\n', 'cd /home/jkligel/bin; bash gitPush']) 
+    script_file.writelines(['#!/usr/bin/env bash\n', 'git config --global credential.helper "cache --timeout 7200"\n\n', 'cd /home/jkligel/bin; bash gitPush\n']) 
     
     for file in glob.iglob(f'{search_dir}/**/.git', recursive=True):
         script_file.write(f'cd {os.path.dirname(file)}; bash gitPush\n')
