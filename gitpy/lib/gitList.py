e736d4e2c2077b19508d2522c41a1da0b9ea8683
Joseph Kligel <jkligel@gmail.com>
2020-07-26 23:07:51 -0500

    gitpy to your rescue

--- gitpy/lib/gitList.py
+++ gitpy/lib/gitList.py
@@ -9,8 +9,8 @@ def find_git(search_dir='/home/jkligel/Github'):
     return localRepoList
 
 def write_bash_script(repoDict):
-    with open('pushAll.sh', 'w') as fh:
-        print('Writing pushAll.sh in cwd...')
+    with open(os.path.join(os.path.dirname(__file__), 'pushAll.sh'), 'w') as fh:
+        print(f'Writing {fh.name}...')
         
         fh.writelines(['#!/usr/bin/env bash\n', 'git config --global credential.helper "cache --timeout 7200"\n\n', 'cd /home/jkligel/bin; bash gitPush\n']) 
         
@@ -18,12 +18,12 @@ def write_bash_script(repoDict):
             fh.write(f'git clone http://github.com/zigjag/{i.lower()}.git\n')  
 
 def write_local_list(repoDict):
-    with open('localRepoList.json', 'w') as fh:
-        print('Writing localRepoList.json in cwd...')
+    with open(os.path.join(os.path.dirname(__file__), 'localRepoList.json'), 'w') as fh:
+        print(f'Writing {fh.name}')
         json.dump(repoDict, fh, indent=4)   
     
 def main():
-    repoDict = find_git()
+    repoDict = find_git('/home/joseph/Github')
     write_bash_script(repoDict)
     write_local_list(repoDict)
 
