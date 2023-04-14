e35739765e06b05567f043891fba97e6e64b28ca
Joseph Kligel <jkligel@gmail.com>
2023-04-14 10:12:50 -0500

    Using GitProject program to push

--- GitProject/main.py
+++ GitProject/main.py
@@ -4,32 +4,44 @@ from find import find
 import sys
 import os
 
+# Global variables
+local_repo_directory = '/home/jkligel/GitHub'
+
+# Main functions called from options provided to this script
+
+## Find all local git directories and return them as a list
 def get_local_repos(search_path='.'):
     return find('.git', search_path)
 
+## Clone all repos from the GithubRepos object automatically to the current directory
 def cloneAll(destination='.'):
     repos = GithubRepos()
     for repoUrl in repos.repoUrls:
         Git.clone(repoUrl, destination)
 
+## Set token for GitHub interaction. Depricated
 def setToken(local_repos):
     for repo in local_repos:
         Git.setToken(repo)
 
+## Add all, commit, and push the changes of the local repos remotely
 def pushAll(local_repos):
     for repo in local_repos:
         Git.push(repo)
 
+## Pull changes from remote repos to the local repos
 def pullAll(local_repos):
     for repo in local_repos:
         Git.pull(repo)
 
-def main():
-    pass
-
+# Execute the code below if main.py was called directly
 if __name__ == '__main__':
-    local_repo_directory = '/home/jkligel/Github'
+    # Create list of local repos from the get_local_repos function
     local_repos = get_local_repos(local_repo_directory)
+
+    # Ensure length of arguments is 2 or more
+    # Then check the argument for the provided keyword
+    # and execute the function corresponding to the keyword 
     if len(sys.argv) >= 2:
         argument = sys.argv[1]
         if argument == 'set-token':
